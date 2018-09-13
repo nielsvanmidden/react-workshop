ee70f1479e3eb39ef481b73262547aa813872b9c
Niels van Midden <nvanmidden@bol.com>
2018-09-13 13:15:05 +0200

    Remove redundant code

--- 4-separation-of-concerns/src/js/store/enhancers/index.js
+++ 4-separation-of-concerns/src/js/store/enhancers/index.js
@@ -1,14 +1,11 @@
 import viewportEnhancer from './enhancer-viewport';
 import keyPressControlEnhancer from './enhancer-global-controls';
 
-export default ({ calculateInitialState = true } = {}) =>
+export default () =>
   createStore => (...args) => {
     const store = createStore(...args);
 
     if (typeof window !== 'undefined') {
-      // adds event listener
-      // that dispatches an action when breakpoints changes
-      viewportEnhancer({ store, calculateInitialState });
       // Adds keydown event listener
       // that dispatches actions on global key events
       keyPressControlEnhancer({ store });
