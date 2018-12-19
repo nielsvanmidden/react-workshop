177a3174294c4ba194099bb8f77eb53b2aada30e
Kasper Peulen <kasperpeulen@gmail.com>
2018-12-19 12:41:09 +0100

    add the store to the app so that tests can run

--- 2-redux/src/js/__tests__/index.test.js
+++ 2-redux/src/js/__tests__/index.test.js
@@ -1,4 +1,14 @@
-import App from '../__stories__/index.stories';
+import React from "react";
+
+import Toaster from "../components/toaster";
+import Store from "../store";
+import { setPristine } from "../store/actions";
+import data from "../__data__";
+
+const store = new Store();
+store.dispatch(setPristine({ data }));
+
+const App = (<div><Toaster store={store}/></div>);
 
 describe('<App />', () => {
   describe('Snaphot', () => {
